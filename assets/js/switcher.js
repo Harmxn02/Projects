225f10aca373683af0e1bf488358ec6118e4bacd
Harman Singh <harman.singh@student.howest.be>
2023-01-12 18:24:43 +0100

    initial commit

---
+++ assets/js/switcher.js
@@ -0,0 +1,35 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", setActive );
+
+
+function setActive() {
+    const $work = document.querySelector("#work");
+    const $about = document.querySelector("#about");
+
+    const $work_section = document.querySelector("#work_section");
+    const $about_section = document.querySelector("#about_section");
+
+
+    $about_section.classList.add("hidden");
+    $work.classList.add("selected");
+
+
+    $work.addEventListener("click", function() {
+        console.log("work clicked");
+        $work.classList.add("selected");
+        $about.classList.remove("selected");
+
+        $about_section.classList.add("hidden");
+        $work_section.classList.remove("hidden");
+    })
+    
+    $about.addEventListener("click", function() {
+        console.log("about clicked");
+        $about.classList.add("selected");
+        $work.classList.remove("selected");
+
+        $work_section.classList.add("hidden");
+        $about_section.classList.remove("hidden");
+    })
+}
